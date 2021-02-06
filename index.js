1bcf3da5cc62ecaa879b381c252606597ec49f59
Milton <milton@aiboo.com.br>
2021-02-05 21:31:49 -0300

    Get types direcly from plasmDefinitions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const { ApiPromise, WsProvider } = require('@polkadot/api')
 const { Keyring } = require('@polkadot/keyring')
-//const plasmDefinitions = require('@plasm/types/interfaces/definitions');
+const plasmDefinitions = require('@plasm/types/interfaces/definitions');
 //const plasmDefinitions = require('./myplasm-types/src/interfaces/definitions');
 const jsonDefinitions = require('./types.js')
 const fs = require('fs')
@@ -15,24 +15,24 @@ async function startChain() {
   console.log("trying connection to ", process.env.RPC_URL)
   const targetAddress = process.env.TARGET_ADDRESS
   const provider = new WsProvider(process.env.RPC_URL)
-  /*const types = Object.values(plasmDefinitions).reduce(
+  const types = Object.values(plasmDefinitions).reduce(
       (res, { types }) => ({ ...res, ...types }),
       {},
   )
 
-  fs.appendFile('generated-types.js', JSON.stringify(types), 'utf-8', (err) => {
+  /*fs.appendFile('generated-types.js', JSON.stringify(types), 'utf-8', (err) => {
     if (err) throw err;
     console.log('The "types" were appended to file!');
   });*/
 
-  /*const api = new ApiPromise({
+  const api = new ApiPromise({
       provider,
       types
-  });*/
-  const api = await ApiPromise.create({
+  });
+  /*const api = await ApiPromise.create({
 	  provider: provider,
 	  types: jsonDefinitions
-  });
+  });*/
 
   api.on('connected', () => console.log(`Connected to ${process.env.RPC_URL}`));
   api.on('disconnected', () => console.log(`Disconnected from ${process.env.RPC_URL}`));
@@ -50,8 +50,8 @@ async function startChain() {
   const fromAmountUnits = new BN('1000000000000000')
   const transfer = api.tx.balances.transfer(targetAddress, fromAmountUnits)      
   // send value
-  const nonce = await api.query.system.accountNonce(process.env.FROM_ADDRESS)
-  //const nonce = await api.rpc.system.accountNextIndex(process.env.FROM_ADDRESS)
+  //const nonce = await api.query.system.accountNonce(process.env.FROM_ADDRESS)
+  const nonce = await api.rpc.system.accountNextIndex(process.env.FROM_ADDRESS)
   console.log("got nonce", nonce)
   const txHash = await transfer.signAndSend(fromPair, {nonce})
 
